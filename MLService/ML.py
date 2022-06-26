37d00476b76df470c1d12eb757e281fa799d682c
Bhavesh Wani <39644919+wanibhavesh@users.noreply.github.com>
2022-06-26 11:11:49 +0200

    Updated testing script

--- MLService/ML.py
+++ MLService/ML.py
@@ -4,32 +4,15 @@ from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder
 import pickle,os
 
-
-
 class ML:
 
     def __init__(self):
-        with open('MLService/models/GradientBoosting_model.pkl','rb') as fp: 
+        with open('MLService/models/classifier.pkl','rb') as fp: 
             self.model=pickle.load(fp)
-
-        with open('MLService/models/le_subject.pkl','rb') as fp: 
-            self.le_subject=pickle.load(fp)
-
-        with open('MLService/models/le_predicate.pkl','rb') as fp: 
-            self.le_predicate=pickle.load(fp)
-
-        with open('MLService/models/le_object.pkl','rb') as fp: 
-            self.le_object=pickle.load(fp)
-
-        
         
     def getEnsembleScore(self, input_df):
         # input_df=pd.DataFrame.from_dict([input_dict])
-        
-        input_df.subject  = self.le_subject.transform(input_df.subject)
-        input_df.predicate= self.le_predicate.transform(input_df.predicate)
-        input_df.object   = self.le_object.transform(input_df.object)
-
-        pred=self.model.predict_proba(input_df)[0]
-        
+        pred = []
+        input_df=input_df.drop(['subject','object','predicate'], axis=1)
+        pred=self.model.predict(input_df)
         return pred
