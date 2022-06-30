50c35bcf394e6891f39df84af014bc09bd663517
Bhavesh Wani <39644919+wanibhavesh@users.noreply.github.com>
2022-06-30 19:37:16 +0200

    updated to return df with ensemble score

--- MLService/ML.py
+++ MLService/ML.py
@@ -4,6 +4,8 @@ from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder
 import pickle,os
 import numpy as np
+from OutputService.Output import Output
+
 class ML:
 
     def __init__(self):
@@ -12,12 +14,18 @@ class ML:
         with open('MLService/models/le_predicate.obj','rb') as fp: 
             self.le_predicate=pickle.load(fp)
 
-    def getEnsembleScore(self, input_df):
-        # input_df=pd.DataFrame.from_dict([input_dict])
-        pred = []
+    def getEnsembleScore(self, assertionScores,approaches):
+
+        op = Output()
+        input_df = op.getOutput(assertionScores,approaches)
+
+        ensembleScore = []
         input_df['predicate'] = input_df['predicate'].map(lambda s: '<unknown>' if s not in self.le_predicate.classes_ else s)
         self.le_predicate.classes_ = np.append(self.le_predicate.classes_, '<unknown>')
-        input_df.predicate= self.le_predicate.transform(input_df.predicate)
-        input_df=input_df.drop(['subject','object'], axis=1)
-        pred=self.model.predict(input_df)
-        return pred
+        input_df.predicate = self.le_predicate.transform(input_df.predicate)
+        input_df = input_df.drop(['subject','object'], axis=1)
+        ensembleScore = self.model.predict(input_df)
+        
+        input_df['ensemble_score'] = ensembleScore
+
+        return(input_df)
