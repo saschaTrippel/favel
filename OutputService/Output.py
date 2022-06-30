2af16bb2a09d2c0026d37ff66ff3a663fa5733a6
Sascha Trippel <strippel@mail.uni-paderborn.de>
2022-06-30 14:11:21 +0200

    Fix and added TODOs to OutputService

--- OutputService/Output.py
+++ OutputService/Output.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import csv
+import logging
 from OutputService.GerbilFormat import GerbilFormat
 from MLService.ML import ML
 
@@ -10,7 +11,8 @@ class Output(GerbilFormat):
 		self.approaches = approaches
 
 	def getOutput(self):
-
+		# TODO: this should not be in output service,
+		# output service should only write stuff to disk, not prepocess stuff for MLService
 		"""
 		To create the output DataFrame that consists of triples and scores from each approach for that particular triple.
 		Returns a DataFrame that would be used by MLService to give the Ensemble score
@@ -26,10 +28,13 @@ class Output(GerbilFormat):
 			result['object'] += self.parseLine(assertionScore.object)
 
 			for approach in self.approaches.keys():
-				if str(approach) in result:
-					result[str(approach)].append(assertionScore.score[str(approach)])
-				else:
-					result[str(approach)] = [assertionScore.score[str(approach)]]
+				try:
+					if str(approach) in result:
+						result[str(approach)].append(assertionScore.score[str(approach)])
+					else:
+						result[str(approach)] = [assertionScore.score[str(approach)]]
+				except KeyError as ex:
+					pass
 
 		df = pd.DataFrame(result)
 		dff = self.ensembleScores(df)
@@ -45,6 +50,7 @@ class Output(GerbilFormat):
 		return(line.split(' '))
 
 	def ensembleScores(self, df):
+		# TODO: MLService should be called in Favel.py, not in OutputService
 		"""
 		To get ensemble score from ML Service.
         """
