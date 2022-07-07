76689933bc89a43068ebde9e01ac02b265be0bb3
Vaibhav Chaudhary <49322269+vaibhavc99@users.noreply.github.com>
2022-07-07 23:37:49 +0200

    Moved DataFrame creation script to MLService

--- MLService/ML.py
+++ MLService/ML.py
@@ -4,7 +4,6 @@ from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder
 import pickle,os
 import numpy as np
-from OutputService.Output import Output
 
 class ML:
 
@@ -14,11 +13,36 @@ class ML:
         with open('MLService/models/le_predicate.obj','rb') as fp: 
             self.le_predicate=pickle.load(fp)
 
+    def createDataFrame(self,assertionScores,approaches):
+        """
+        To create the DataFrame that consists of triples and scores from each approach for that particular triple.
+        """
+        result = dict()
+        result['subject'] = []
+        result['predicate'] = []
+        result['object'] = []
+
+        for assertionScore in assertionScores:
+            result['subject'].append(assertionScore.subject)
+            result['predicate'].append(assertionScore.predicate)
+            result['object'].append(assertionScore.object)
+
+            for approach in approaches.keys():
+                try:
+                    if str(approach) in result:
+                        result[str(approach)].append(assertionScore.score[str(approach)])
+                    else:
+                        result[str(approach)] = [assertionScore.score[str(approach)]]
+                except KeyError as ex:
+                    pass
+
+        df = pd.DataFrame(result)
+        
+        return(df)
+
     def getEnsembleScore(self, assertionScores,approaches):
 
-        # TODO: must not make a call to OutputService, otherwise services would be coupled
-        op = Output()
-        input_df = op.getOutput(assertionScores,approaches)
+        input_df = self.createDataFrame(assertionScores,approaches)
 
         ensembleScore = []
         input_df['predicate'] = input_df['predicate'].map(lambda s: '<unknown>' if s not in self.le_predicate.classes_ else s)
