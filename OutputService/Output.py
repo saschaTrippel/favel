0af8103d72089ca8b5786d24fe4c26f005dc69ab
Vaibhav Chaudhary <49322269+vaibhavc99@users.noreply.github.com>
2022-07-08 01:02:55 +0200

    Fix

--- OutputService/Output.py
+++ OutputService/Output.py
@@ -4,44 +4,17 @@ from OutputService.GerbilFormat import GerbilFormat
 
 class Output():
 
-	def getOutput(self,assertionScores,approaches):
-		"""
-		To create the output DataFrame that consists of triples and scores from each approach for that particular triple.
-        """
-		result = dict()
-		result['subject'] = []
-		result['predicate'] = []
-		result['object'] = []
-
-		for assertionScore in assertionScores:
-			result['subject'].append(assertionScore.subject)
-			result['predicate'].append(assertionScore.predicate)
-			result['object'].append(assertionScore.object)
-
-			for approach in approaches.keys():
-				try:
-					if str(approach) in result:
-						result[str(approach)].append(assertionScore.score[str(approach)])
-					else:
-						result[str(approach)] = [assertionScore.score[str(approach)]]
-				except KeyError as ex:
-					pass
-
-		df = pd.DataFrame(result)
-		
-		return(df)
-
 	def writeOutput(self,df):
 		"""
 		Writes results to file.
         """
 		df.to_csv("./OutputService/Outputs/Output.csv",index=False)
 
-	def gerbilFormat(self):
+	def gerbilFormat(self,testingData):
 		"""
-		To convert datasets used by Favel into GERBIL format.
-		Also, converts output from Favel to GERBIL format for Evaluation purposes.
+		To convert testingData into GERBIL format.
+		Also, converts output(ensemble_score) from Favel to GERBIL format for Evaluation purposes.
         """
-		g = GerbilFormat()
+		g = GerbilFormat(testingData)
 		g.getGerbilFormat()
 
