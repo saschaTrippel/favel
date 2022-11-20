9cf2bb7fa039c99a3927668e2da6b433d0ab06af
ltphen <tatkeulionel@gmail.com>
2022-11-20 14:33:56 +0100

    First working version of containers

--- Software/test/test_containers.py
+++ Software/test/test_containers.py
@@ -7,12 +7,20 @@ class TestContainers(unittest.TestCase):
         self.containers = Containers()
 
     def testStartContainers(self):
-       # self.containers.startContainers()
+        self.containers.startContainers()
         containers = self.containers.docker.compose.ps()
         self.assertGreaterEqual(len(containers), 1)
 
     def testStopContainers(self):
-        # self.containers.startContainers()
+        # self.containers.stopContainers()
+        containers = self.containers.docker.compose.ps()
+        self.assertEqual(len(containers), 0)
+
+    def testStatusContainers(self):
+        pass
+
+    def testRemoveContainers(self):
+        self.containers.rmContainers()
         containers = self.containers.docker.compose.ps()
         self.assertEqual(len(containers), 0)
 
