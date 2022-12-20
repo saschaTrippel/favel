62a7644afa0413130498d4919492d8229da27d93
Sascha Trippel <strippel@mail.uni-paderborn.de>
2022-12-20 11:11:19 +0100

    Added script to create plots from overview

---
+++ Analysis/Analysis.py
@@ -0,0 +1,37 @@
+from os import path
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def loadPaths():
+    paths = dict()
+    favelPath = path.realpath(__file__)
+    pathLst = favelPath.split('/')
+    favelPath = "/".join(pathLst[:-2])
+    
+    paths["Overview"] = path.join(favelPath, "Evaluation/Overview.xlsx")
+    paths["Analysis"] = path.join(favelPath, "Analysis/")
+    return paths
+    
+
+def readOverview():
+    return pd.read_excel(PATHS["Overview"])
+
+def getBpdp(df):
+    return df.loc[df['Dataset'] == "BPDP_Dataset"]
+
+def getFactBench(df):
+    return df.loc[df['Dataset'] == "factbench-clean"]
+
+def getFavel(df):
+    return df.loc[df['Dataset'] == "FinalDataset_Hard"]
+
+def plotPerformanceStdDev(df):
+    df = df[["Testing AUC-ROC Mean", "Testing AUC-ROC Std. Dev."]]
+    plot = df.plot(x="Testing AUC-ROC Mean", y="Testing AUC-ROC Std. Dev.", kind="scatter")
+    fig = plot.get_figure()
+    fig.savefig(path.join(PATHS["Analysis"], "performance-stdDev.pdf"))
+    
+PATHS = loadPaths()
+
+df = readOverview()
+plotPerformanceStdDev(df)
